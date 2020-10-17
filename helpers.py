e228e128fbdddeba3417c20188514ebbfefa12da
Marcin Świerczyna <polpusty@gmail.com>
2020-10-17 17:22:51 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -8,6 +8,6 @@ def download_model(url, save_path):
     Download model files.
     """
     r = requests.get(url)
-    bytesio_object = io.StringIO(r.content)
+    bytesio_object = io.StringIO(r.text)
     with open(save_path, "w+") as f:
         f.write(bytesio_object.getbuffer())

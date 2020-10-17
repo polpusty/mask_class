989acd5c076a71749d43677c07ee4ebef4d57411
Marcin Świerczyna <polpusty@gmail.com>
2020-10-17 17:52:23 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -8,5 +8,5 @@ def download_model(url, save_path):
     Download model files.
     """
     r = requests.get(url)
-    with open(save_path, "w+") as f:
-        f.write(r.text)
+    with open(save_path, "wb") as f:
+        f.write(r.content)
